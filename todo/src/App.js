24c185b504311b88e8382f843bc44e342aa70e08
Krisli Dimo <krislidimo@gmail.com>
2019-05-14 17:55:15 -0500

    added components to App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Title from './components/Title.js'
+import TodoList from './components/TodoList.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Title />
+      <TodoList />
     </div>
   );
 }
